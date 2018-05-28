d10c41e98ead7b17db8c27e3bd48b96eec704a91
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-05-28 14:22:49 +0200

    Por terminar

--- javascript/js/contactos.js
+++ javascript/js/contactos.js
@@ -1,31 +1,90 @@
 class Contacto
 {
-	constructor(Nombre, Telefono, Direccion)
+	// -n: Nombre, t: Telefono, d: Direccion
+	constructor(n, t, d)
 	{
-		this.nombre = Nombre;
-		this.telefono = Telefono;
-		this.direccion = Direccion;
+		this.nombre = n;
+		this.telefono = t;
+		this.direccion = d;
 	}
 
-	Escribir()
+	Escribir(elementoHTML)
 	{
 		var resultado;
 
-		resultado = document.getElementById('Resultado');
+		resultado = document.getElementById(elementoHTML);
 		resultado.innerHTML +=
 		"Nombre: " + this.nombre + "<br>" +
 		"Telefono: " + this.telefono + "<br>" +
 		"Direccion: " + this.direccion + "<br>";
 	}
 
-	Cambiar(NuevoNombre, NuevoTelefono, NuevaDireccion)
+	EscribirNombre(elementoHTML)
 	{
-		this.nombre = NuevoNombre;
+		var resultado;
+
+		resultado = document.getElementById(elementoHTML);
+		resultado.innerHTML +=
+		"Nombre: " + this.nombre + "<br>"; 
+	}
+
+	CambiarTelefono(NuevoNombre, NuevoTelefono, NuevaDireccion)
+	{
 		this.telefono = NuevoTelefono;
+		
+	}
+	CambiarDireccion(elementoHTML)
+	{
 		this.direccion = NuevaDireccion;
 	}
 }
 
+class ListaContactos
+{
+	constructor()
+	{
+		this.nContactos = 0;
+		this.lista = new Array(5);
+	}
+
+	InsertarContacto(nom, tel, dir)
+	{
+		var contacto = new Contacto(nom, tel, dir);
+
+		this.lista[this.nContactos] = contacto;
+		this.nContactos ++;
+	}
+
+	EscribirNombre(elementoHTML)
+	{
+		// Escribir elementos existentes en la lista.
+		//lista. El número de elementos se almacena en this.nContactos
+		var resultado = document.getElementById(elementoHTML);
+		resultado.innerHTML = "";
+		for (var i = 0; i < this.nContactos; i++) 
+		{
+			this.lista[i].EscribirNombre(elementoHTML);
+		}
+	}
+}
+
+var miLista = new ListaContactos();
+
+function InsertarContactoLista()
+{
+	var miNombre,
+		miTelefono,
+		miDireccion;
+
+	miNombre = document.getElementById('Nombre').value;
+	miTelefono = document.getElementById('Telefono').value;
+	miDireccion = document.getElementById('Direccion').value;
+
+	miLista.InsertarContacto(miNombre,miTelefono,miDireccion);
+	miLista.EscribirNombre('ListaContactoNombre');
+}
+
+/*
 var c = new Array();
 	c[0] = new Contacto("Isco", 657483927, "Madrid");
 	c[1] = new Contacto("Cristiano", 657489389, "Lisboa");
@@ -74,3 +133,4 @@ function AñadirContacto()
                     c[i].Escribir();
 }
 }
+*/
